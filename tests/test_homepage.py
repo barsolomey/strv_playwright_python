f8871d3c98f182f96221a93f69523ea308f7cb30
Artem Belov <artem.belov@strv.com>
2024-05-15 15:39:25 +0200

    remove redundant imports

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,14 +1,10 @@
-import re
 from playwright.sync_api import Page, expect
 
 import pages.homepage
-from pages import homepage
 from locators import homepage_locators
-from pages.homepage import HomePage
-from urls import urls
 
 
-def test_has_title(page: Page):
+def test_check_title(page: Page):
     # Go to STRV homepage and check that homepage url matches
     homepage = pages.homepage.HomePage(page)
     homepage.navigate()
