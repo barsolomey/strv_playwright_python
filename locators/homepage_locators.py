d44fbcefe144425c97a65b2a7b6edf01df6d72e0
Artem Belov <artem.belov@strv.com>
2024-05-15 15:31:53 +0200

    add homepage locators

---
+++ locators/homepage_locators.py
@@ -0,0 +1,6 @@
+import re
+
+
+class HomePageLocators:
+    header_text_nextlevel = '//*[@id="__next"]/div[1]/main/section[1]/span/span/span/span/span[1]'
+    homepage_title = 'STRV - Next-Level Design and Engineering Team'
