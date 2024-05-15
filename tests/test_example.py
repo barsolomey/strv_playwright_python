c493ecc1a204a4e69b6a09abf66c4db774548efe
Artem Belov <artem.belov@strv.com>
2024-05-15 15:31:11 +0200

    add TestRail requirement, add first test

--- tests/test_example.py
+++
@@ -1,8 +0,0 @@
-import re
-from playwright.sync_api import Page, expect
-
-def test_has_title(page: Page):
-    page.goto("https://strv.com")
-
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("STRV - Next-Level Design and Engineering Team"))
